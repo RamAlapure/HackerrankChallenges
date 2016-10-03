90c83d3c816dc5399d2af5f24948018367ba8dce
Ram Alapure <alapureram@gmail.com>
2016-10-03 14:43:01 +0530

    Create DiagonalDifference.java

---
+++ DiagonalDifference.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n*n];
+        int d1 = 0;
+        int d2 = 0;
+        for(int i=0; i<n*n; i++){
+            a[i] = scan.nextInt();
+        }
+        for(int i=0; i<n; i++){
+           d1 += a[i+i*n];
+           d2 += a[(n-1)*(i+1)];
+        }
+        
+        int d = Math.abs(d1-d2);
+        System.out.println(d);
+        
+    }
+}
