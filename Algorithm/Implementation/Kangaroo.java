fa7ce21ff5578c99d90e0f13e83c779c5e576bff
Ram Alapure <alapureram@gmail.com>
2016-10-07 01:25:17 +0530

    Update Kangaroo.java
    There are two kangaroos on an x-axis ready to jump in the positive direction (i.e, toward positive infinity). 
    The first kangaroo starts at location x1 and moves at a rate of v1 meters per jump. 
    The second kangaroo starts at location x2 and moves at a rate of v2 meters per jump. 
    Given the starting locations and movement rates for each kangaroo, 
    can you determine if they'll ever land at the same location at the same time?
    
    Input Format
    
    A single line of four space-separated integers denoting the respective values of x1, v1, x2, and v2.
    
    Constraints:
    0 <= x1 < x2 <= 10000
    0 <= v1,v2 <= 10000
    
    Output Format
    
    Print YES if they can land on the same location at the same time; otherwise, print NO.
    
    Note: The two kangaroos must land at the same location after making the same number of jumps.

--- Algorithm/Implementation/Kangaroo.java
+++ Algorithm/Implementation/Kangaroo.java
@@ -1,4 +1,27 @@
-//wrong answer
+/*
+There are two kangaroos on an x-axis ready to jump in the positive direction (i.e, toward positive infinity). 
+The first kangaroo starts at location x1 and moves at a rate of v1 meters per jump. 
+The second kangaroo starts at location x2 and moves at a rate of v2 meters per jump. 
+Given the starting locations and movement rates for each kangaroo, 
+can you determine if they'll ever land at the same location at the same time?
+
+Input Format
+
+A single line of four space-separated integers denoting the respective values of x1, v1, x2, and v2.
+
+Constraints:
+0 <= x1 < x2 <= 10000
+0 <= v1,v2 <= 10000
+
+Output Format
+
+Print YES if they can land on the same location at the same time; otherwise, print NO.
+
+Note: The two kangaroos must land at the same location after making the same number of jumps.
+
+*/
+
+
 
 import java.io.*;
 import java.util.*;
@@ -15,25 +38,15 @@ public class Solution {
         int x2 = in.nextInt();
         int v2 = in.nextInt();
         
-        if((x2>x1 && v2>v1) || (x2<x1 && v2<v1)){
-            System.out.println("NO");
-        }
-        
-        int l = (x1+v1)*(x2+v2);
-        
-        for(int i=0; i<l; i++){
-            x1 += v1;
-            x2 += v2;
-            if(x1 == x2){
+        if((x1<x2) && (v1<v2))
+        System.out.println("NO");
+        else
+        {        
+            if((v1!=v2) && ((x2-x1)%(v1-v2))==0)
                 System.out.println("YES");
-                i = 20001;
-            }
+            else
+                System.out.println("NO");
+
         }
-        if((x2>x1 && v2>v1) || (x2<x1 && v2<v1)){
-            
-        }else{ 
-            if(x1 != x2) System.out.println("NO");
-        }
-        
     }
 }
