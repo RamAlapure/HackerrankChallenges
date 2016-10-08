197a49b7de0d6fa42f83ff140a497f7c602da32c
Ram Alapure <alapureram@gmail.com>
2016-10-09 00:33:40 +0530

    Day 3: Intro to Conditional Statements

--- 30-Days-of-Code/ConditionalStatements.java
+++ 30-Days-of-Code/ConditionalStatements.java
@@ -10,7 +10,7 @@ public class Solution {
       int n = scan.nextInt(); 
       scan.close();
       String ans="";
-          
+      
       // if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
       if((n%2==1) || ((n%2==0) && (n>=6 && n<=20))){
          ans = "Weird";
