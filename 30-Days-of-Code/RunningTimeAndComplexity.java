500e16bd027f882e543385ab4d07a2850f983e49
Ram Alapure <alapureram@gmail.com>
2016-10-23 13:06:56 +0530

    Day 25: Running Time and Complexity

--- 30-Days-of-Code/RunningTimeAndComplexity.java
+++ 30-Days-of-Code/RunningTimeAndComplexity.java
@@ -7,20 +7,25 @@ public class Solution {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
+        
         for(int i=0; i<n; i++){
             int num = scan.nextInt();
+            int k = (int)Math.sqrt(num);
             int count=0;
-            for(int j=2; j<(num/2); j++){
-                if(num%j==0) {
-                    count++;
-                    j=num/2;
+            if(num<2) System.out.println("Not prime");
+            else{
+                for(int j=2; j<=k; j++){
+                    if(num%j==0) {
+                        count++;
+                        j=k;
+                    }
                 }
-            }
-            if(count<1){
-                System.out.println("Prime");
-            }else{
-                System.out.println("Not prime");
-            }
+                if(count<1){
+                    System.out.println("Prime");
+                }else{
+                    System.out.println("Not prime");
+                }
+            }            
         }
     }
 }
